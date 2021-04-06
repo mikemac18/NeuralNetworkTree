9e8a0a4aa6de0fc9541e79aa5d78262867a693a1
windchasm <60714486+windchasm@users.noreply.github.com>
2021-04-05 22:15:16 -0400

    Vivek's performance tests on the bulk neural network.
    "Stopwatch" testing as well as memory tests done on the bulk neural network.

---
+++ 3) Vivek Performance Tests (as of 05-Apr-2021)/Bulk_NN (highest accuracy).py
@@ -0,0 +1,67 @@
+import numpy as np
+import pandas as pd
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.utils import np_utils
+
+dataframe = pd.read_csv("Vivek_Ranged_Data.csv", delimiter=",", header=None)
+data = dataframe.values
+
+# Loading the data on which the neural network will be trained. 
+x_trn = data[:9000,0:5]
+y_trn = data[:9000,5]
+x_tst = data[9001:10000,0:5]
+y_tst = data[9001:10000,5]
+
+# One hot encoding
+dummy_y_trn = np_utils.to_categorical(y_trn)
+dummy_y_trn = np.delete(dummy_y_trn, obj=0, axis=1)
+
+# Function that creates our neural network
+def bulk_NN():
+    # Creating model
+    model = Sequential()
+   
+    # Adding the input layer, using the sigmoid actiation function.
+    model.add(Dense(753, input_dim=5, activation='sigmoid'))
+    
+    # Adding 7 hidden layers, all using the sigmoid activation function.
+    model.add(Dense(336, activation='sigmoid'))
+    model.add(Dense(184, activation='sigmoid'))
+    model.add(Dense(377, activation='sigmoid'))
+    model.add(Dense(466, activation='sigmoid'))
+    model.add(Dense(143, activation='sigmoid'))
+    model.add(Dense(519, activation='sigmoid'))
+    model.add(Dense(654, activation='sigmoid'))
+
+    # Adding the output layer, using the softmax activation function. The number of units must be 27 here.
+    model.add(Dense(27, activation='softmax'))
+    
+    # Compile the model, using the 'adam' optimizer.
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
+
+# Function for predicting labels using keras predict function
+def predict(model,x):
+    preds = np.argmax(model.predict(x), axis=-1)
+    for i in range(len(preds)):
+        preds[i] = preds[i] + 1
+    return preds
+
+# Function for testing accuracy of predictions
+def testAccuracy(preds, labels):
+    count = 0
+    for i in range(len(preds)):
+        if(preds[i] == labels[i]):
+            count += 1
+    final = 100*(count/len(labels))
+    return print("Accuracy: %.2f" % (final))
+
+model = bulk_NN()
+
+# Fitting our model with the training data
+model.fit(x_trn, dummy_y_trn, epochs=197, batch_size=79, verbose=0)
+
+# Testing accuracy after training
+predictions = predict(model,x_tst)
+testAccuracy(predictions,y_tst)
